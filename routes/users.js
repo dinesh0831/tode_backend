feda669e61fe82f877fc780fe78c8bb37543e9dd
Dinesh <81325951+dinesh0831@users.noreply.github.com>
2021-12-16 12:28:20 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,6 @@ const service=require("../module/user.service")
 
 router.post("/register",service.register)
 router.post("/login",service.login );
-router.post("/forget_password",service.forgetPassword)
-router.patch("/reset",service.resetPassword)
+
 
 module.exports=router
